39477b0dd1a48021dc38801fb2be29f3e7e57852
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-29 12:48:56 +0100

    Mise à jour de la fonction var_dumpj dans functions.php

--- functions.php
+++ functions.php
@@ -11,20 +11,20 @@ if (!function_exists('secur')) {
 ?>
 
 <?php
-//joli var dump
-function var_dumpj($d, $e = null)
-{
-  echo "
-<pre><code>";
-  if ($e == "e") {
-    echo $d;
-  } else {
-    if (is_array($d)) {
-      print_r($d);
+if (!function_exists('var_dumpj')) {
+  function var_dumpj($d, $e = null)
+  {
+    echo "<pre><code>";
+    if ($e == "e") {
+      echo $d;
     } else {
-      var_dump($d);
+      if (is_array($d)) {
+        print_r($d);
+      } else {
+        var_dump($d);
+      }
     }
+    echo "</code></pre>";
   }
-  echo "</code></pre>";
 }
 ?>
