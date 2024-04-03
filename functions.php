96a077c1e4c727964c5d1e65417d52eb254e21f1
Mirko Bozzetto <mirkouch@gmail.com>
2024-04-03 11:29:38 +0200

    Mise à jour des fonctions de sécurité et de débogage

--- functions.php
+++ functions.php
@@ -1,28 +1,26 @@
 <?php
-if (!function_exists('secur')) {
-  function secur($data)
-  {
-    $data = trim($data); // enlever tous les espaces comme tabs ou plusieurs espaces d'affilé
-    $data = stripslashes($data); // enlever les backslashes ou \\ deviennent des \
-    $data = htmlspecialchars($data); // convertis les caractères spéciaux en balises HTML
-    return $data;
-  }
+
+function secur($data)
+{
+  $data = trim($data); // enlever tous les espaces comme tabs ou plusieurs espaces d'affilé
+  $data = stripslashes($data); // enlever les backslashes ou \\ deviennent des \
+  $data = htmlspecialchars($data); // convertis les caractères spéciaux en balises HTML
+  return $data;
 }
 ?>
 
 <?php
-if (!function_exists('var_dumpj')) {
-  function var_dumpj($d, $e = null)
-  {
-    echo "<pre><code>";
-    if ($e == "e") {
-      echo $d;
+
+function var_dumpj($d, $e = null)
+{
+  echo "<pre><code>";
+  if ($e == "e") {
+    echo $d;
+  } else {
+    if (is_array($d)) {
+      print_r($d);
     } else {
-      if (is_array($d)) {
-        print_r($d);
-      } else {
-        var_dump($d);
-      }
+      var_dump($d);
     }
     echo "</code></pre>";
   }
